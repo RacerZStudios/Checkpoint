9bf91725ec2bf15ed02412c146ae186cbf0777d3
Zac <zac.bogner@gmail.com>
2021-05-01 15:04:21 -0700

    Player Camera follow, and intro text for FTUE

---
+++ Assets/Scripts/PlayerFollow.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour
+{
+    public Transform player;
+    public float cameraPos; 
+
+    private void FixedUpdate()
+    {
+        Vector3 pos = player.position;
+        transform.position = new Vector3(player.position.x, player.position.y + cameraPos, player.position.z); 
+    }
+}
