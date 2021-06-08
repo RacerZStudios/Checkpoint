23e2e9627263f4999120b185c22365851030316a
Zac <zac.bogner@gmail.com>
2021-06-07 21:38:26 -0700

    Update Player Rotation when enabling shield
    * Set local rotation of player when shield active

--- Assets/Scripts/Shield_Controller.cs
+++ Assets/Scripts/Shield_Controller.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Shield_Controller : MonoBehaviour
 {
     public GameObject shieldObj;
+    public GameObject player;
+    public Quaternion PlayerRot; 
 
     private void Update()
     {
         if(Input.GetKey(KeyCode.E))
         {
+            player.gameObject.transform.rotation = PlayerRot; 
             shieldObj.gameObject.SetActive(true); 
         }
         else if(!Input.GetKey(KeyCode.E))
